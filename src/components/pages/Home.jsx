d83dc23bc94de86316081237720c55455ef3f101
CDesiree <desireecastrocpe@gmail.com>
2024-01-21 21:54:43 +0800

    [feat] add home page

---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Home() {
+    return (
+        <>
+            <div className="home-page">
+                Hello des
+            </div>
+        </>
+    );
+}
+
+export default Home;
