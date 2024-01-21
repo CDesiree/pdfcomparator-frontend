64d4af89b1b7ecdb4954912518218c5d3e7ad54c
CDesiree <desireecastrocpe@gmail.com>
2024-01-21 21:54:29 +0800

    [feat] add standard layout for pages

---
+++ src/components/layout/SiteLayout.jsx
@@ -0,0 +1,20 @@
+import { Layout } from "antd";
+import { Content, Header, Footer } from "antd/es/layout/layout";
+import React from "react";
+import { Outlet } from "react-router";
+import "./SiteLayout.css"
+
+function SiteLayout() {
+    return (
+        <Layout>
+            <Header className="sitelayout-header">DPMC Comparator Tool</Header>
+            <Content className="sitelayout-bg">Body
+                <Outlet />
+            </Content>
+            <Footer className="sitelayout-footer">DPMC {new Date().getFullYear()} Created by CASTRDE</Footer>
+        </Layout>
+    )
+
+}
+
+export default SiteLayout;
