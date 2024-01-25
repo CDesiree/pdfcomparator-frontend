a2754551fd81cedf0c678ba57a758d120adbf138
CDesiree <desireecastrocpe@gmail.com>
2024-01-25 23:12:50 +0800

    [feat] add input to send pdf to backend

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,10 +1,38 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 
 function Home() {
+    const [file1, setFile1] = useState(null);
+    const [file2, setFile2] = useState(null);
+
+    const handleFile1 = (event, fileKey) => {
+        setFile1(event.target.files[0]);
+    }
+
+    const handleFile2 = (event, fileKey) => {
+        setFile2(event.target.files[0]);
+    }
+     //To do: separate axios
+    const handleCompare = () => {
+        const formData = new FormData();
+        formData.append("file1", file1);
+        formData.append("file2", file2);
+
+        axios.post("http://localhost:8080/api/pdf/compare", formData)
+            .then(response => {
+                console.log(response.data);
+            })
+            .catch(error => {
+                console.error("Error: ", error);
+            })
+    }
+
     return (
         <>
             <div className="home-page">
-                Hello des
+                <input type="file" onChange={handleFile1} />
+                <input type="file" onChange={handleFile2} />
+                <button onClick={handleCompare}>Compare</button>
             </div>
         </>
     );
